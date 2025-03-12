3cf7a1c460956cd9296a0d2f5dae92e3b452e3d2
Erick0105 <erickaxs0105@gmail.com>
2025-03-12 17:48:35 -0300

    verificação de inteiro criado, e limpeza do terminal também

--- jogo.py
+++ jogo.py
@@ -1,5 +1,22 @@
 import random, time, os
 
+def limpar_terminal():
+    sistema = os.name
+    if sistema == 'nt':  
+        os.system('cls')
+    else: 
+        os.system('clear')
+        
+def validar_num(text) -> int:
+    texto_erro = ""
+    while True:
+        try:
+            possivel_num = int(input(texto_erro + text))
+            break
+        except:
+            texto_erro = "Por favor digite um número inteiro\n" 
+    return possivel_num
+
 led_array = [1,2,3,4,5]
 led_array_escolhidos = []
 
@@ -13,13 +30,13 @@ while True:
         contador += 1
         print(f'{contador}º número - {i}')
         time.sleep(1.5)
-        os.system('cls')
+        limpar_terminal()
     
-    valor_digitado = int(input("Qual foi o ultimo valor que apareceu?"))
+    valor_digitado = validar_num("Qual foi o ultimo valor que apareceu?\n=> ")
     if valor_digitado == led_array_escolhidos[-1]:
         print("Parabéns você acertou, vamos continuar")
         time.sleep(2)
-        os.system('cls')
+        limpar_terminal()
     else:
         print("Você errou que pena")
         break
